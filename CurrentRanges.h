5922ddd5f071e2c94e11dff2d92fb776d22c3ed8
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-04-29 17:07:10 +0530

    Update CurrentRanges.h

--- CurrentRanges.h
+++ CurrentRanges.h
@@ -1,39 +1,15 @@
 #pragma once
 
-typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
-} CoolingType;
+
 
 typedef enum {
-  NORMAL,
-  TOO_LOW,
-  TOO_HIGH
+  exception,
+  FAIL,
+  PASS,
+INVALID
 } BreachType;
 
-typedef struct {
-	double lowerLimit;
-	double upperLimit;
-}BreachLimit;
 
+BreachType inferRange(int *value, int range);
 
-typedef enum {
-  TO_CONTROLLER,
-  TO_EMAIL,
-  TO_CONSOLE
-} AlertTarget;
-
-typedef struct {
-  CoolingType coolingType;
-  char brand[48];
-} BatteryCharacter;
-
-typedef enum{
-	FAIL,
-	PASS
-} InfoType;
-InfoType checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
-BreachType inferRange(double value, double lowerLimit, double upperLimit);
 
