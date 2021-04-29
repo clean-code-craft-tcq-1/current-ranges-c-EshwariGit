aac8d7833069651013478f48949f0b63f2377223
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-04-29 12:53:11 +0530

    Update test-currentRanges.cpp

--- test-currentRanges.cpp
+++ test-currentRanges.cpp
@@ -3,9 +3,14 @@
 #include "test/catch.hpp"
 #include "CurrentRanges.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the ranges according to limits -passing testcase") {
   REQUIRE(inferRange(12, 20, 30) == TOO_LOW);
 }
+TEST_CASE("infers the ranges according to not in limits -failing testcase") {
+  REQUIRE(inferRange(12, 20, 30) == TOO_LOW);
+  
+}
+
 
 
 
