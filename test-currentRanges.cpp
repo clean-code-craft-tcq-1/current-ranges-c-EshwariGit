b10a93d26cfbd031bd385d03f6ae56d86b349898
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-04-30 18:13:04 +0530

    Update test-currentRanges.cpp

--- test-currentRanges.cpp
+++ test-currentRanges.cpp
@@ -4,11 +4,22 @@
 #include "CurrentRanges.h"
 #include "stdlib.h"
 #include "math.h"
+// Number of reading betweem 3 to 5 is 4 so testcase shall pass if this is in permitted range
 
+//PASSING TESTCASE
 TEST_CASE("To check if the samples are in range") {
     int CurrentRange[] = {3, 3, 5, 4};
     int listofvalues = sizeof(CurrentRange) / sizeof(CurrentRange[0]);
     int BreachType[] ={3,5};
-  REQUIRE(inferRange(listofvalues,CurrentRange,BreachType) == 3);
+  REQUIRE(inferRange(listofvalues,CurrentRange,BreachType) == 4);
 }
 
+/* FAILING TESTCASE
+TEST_CASE("To check if the samples are out range") {
+    int CurrentRange[] = {3, 3, 5, 4};
+    int listofvalues = sizeof(CurrentRange) / sizeof(CurrentRange[0]);
+    int BreachType[] ={3,5};
+  REQUIRE(inferRange(listofvalues,CurrentRange,BreachType) == 7);
+}
+
+/*
