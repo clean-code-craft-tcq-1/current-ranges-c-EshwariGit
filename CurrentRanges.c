5787c470874d778f66ea740d5ff575311b4abf65
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-04-30 17:59:17 +0530

    Update CurrentRanges.c

--- CurrentRanges.c
+++ CurrentRanges.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 
-BreachType inferRange(int listofvalues,int *CurrentRange,int *BreachType)
+int inferRange(int listofvalues,int *CurrentRange,int *BreachType)
 {
     int i,count = 0;
     for(i=0;i<listofvalues;i++)
